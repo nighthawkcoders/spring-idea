9e6ba04ed3eb37409c24613993570d25a5294dba
John Mortensen <jmortensen@powayusd.com>
2020-12-11 13:26:26 -0800

    Comments and links

--- src/main/java/com/example/demo/Hello.java
+++ src/main/java/com/example/demo/Hello.java
@@ -1,5 +1,7 @@
 package com.example.demo;
-
+/* Basic web site that loads index.html, added is route to /hello
+ * From IntelliJ Tutorial - https://www.jetbrains.com/help/idea/your-first-spring-application.html
+ */
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
@@ -8,17 +10,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication  //https://dzone.com/articles/the-springbootapplication-annotation-example-in-ja#
+@SpringBootApplication  // explained here - https://dzone.com/articles/the-springbootapplication-annotation-example-in-ja#
 @RestController
 public class Hello {
 
     public static void main(String[] args) {
-        SpringApplication.run(Hello.class, args);
+        SpringApplication.run(Hello.class, args);   // Internal magic:
+                                                    // loads index.html page from static folder
+                                                    // run server on localhost:8080
     }
 
-    @GetMapping("/hello")   // annotation for web url
+    @GetMapping("/hello")   // Hello class handles GET request for /hello via @GetMapping annotation
     public String sayHello(@RequestParam(value = "name", defaultValue = "World") String name) {
-        return String.format("Hello %s!", name);    // Simple output of formatted string
+        // @RequestParam handles default values, name is regular Java parameter
+        return String.format("Hello %s!", name);    // This is a simple output of formatted string
     }
 
 }
--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,17 +1,20 @@
 package com.example.demo;
+/* Web Content with Spring MVC
+Spring Example: https://spring.io/guides/gs/serving-web-con
+ */
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@Controller
+@Controller  // HTTP requests are handled a controller, using the @Controller annotation
 public class HelloController {
 
-    @GetMapping("/greeting")
+    @GetMapping("/greeting")    // HelloController handles GET request for /greeting, maps it to greeting() method and variable bindings
     public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
         model.addAttribute("name", name);   // model is passed to html
-        return "greeting";                     // load and renders greeting.html
+        return "greeting";                     // returns name of VIEW (greeting)
     }
 
 }
