6a3bf9cc838b9065558f507211b7e9f248a783d5
John Mortensen <jmortensen@powayusd.com>
2020-12-30 07:49:40 -0800

    Removing result pages, in favor of inline results.

--- src/main/java/com/example/demo/ViewController.java
+++ src/main/java/com/example/demo/ViewController.java
@@ -9,21 +9,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import com.example.demo.technicals.FibStream;
 
-
 @Controller  // HTTP requests are handled a controller, using the @Controller annotation
 public class ViewController {
 
     @GetMapping("/greet")    // CONTROLLER handles GET request for /greeting, maps it to greeting() and does variable bindings
-    public String greeting() {
-        // @RequestParam handles required and default values, name and model are class variables, model looking like JSON
-        return "greet";                     // returns name of VIEW (greeting)
-    }
-
-    @GetMapping("/greeting")    // CONTROLLER handles GET request for /greeting, maps it to greeting() and does variable bindings
     public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
         // @RequestParam handles required and default values, name and model are class variables, model looking like JSON
         model.addAttribute("name", name);   // MODEL is passed to html
-        return "greeting";                     // returns HTML VIEW (greeting)
+        return "greet";                     // returns HTML VIEW (greeting)
     }
 
     @GetMapping("/pbl")   // CONTROLLER handles GET request for
@@ -36,18 +29,11 @@ public class ViewController {
         return "games/snake";                     // returns HTML VIEW (greeting)
     }
 
-    @GetMapping("/fib")    // CONTROLLER handles GET request for /greeting, maps it to greeting() and does variable bindings
-    public String fib() {
-        // @RequestParam handles required and default values, name and model are class variables, model looking like JSON
-        return "technicals/fib";                     // returns name of VIEW (greeting)
-    }
-
-    @GetMapping("/fibnth")   // CONTROLLER handles GET request for
-    public String fibNth(@RequestParam(name="seq", required=false,  defaultValue="20") String seq, Model model) {
+    @GetMapping("/fib")   // CONTROLLER handles GET request for
+    public String fib(@RequestParam(name="seq", required=false,  defaultValue="1") String seq, Model model) {
         FibStream fib = new FibStream(Integer.parseInt(seq));
         model.addAttribute("fib", fib.getNth());        // MODEL is passed to html
         model.addAttribute("fibseq", fib.getNthSeq());  // MODEL is passed to html
-        return "technicals/fibnth";                        // returns HTML VIEW (greeting)
+        return "technicals/fib";                        // returns HTML VIEW (greeting)
     }
-
 }
