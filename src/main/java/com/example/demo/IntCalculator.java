7ce16834f83f76eedae404cd630f484f861081cd
John Mortensen <jmortensen@powayusd.com>
2021-01-03 20:31:19 -0800

    Function calc pass 1

--- src/main/java/com/example/demo/IntCalculator.java
+++ src/main/java/com/example/demo/IntCalculator.java
@@ -3,42 +3,49 @@ package com.example.demo;
 import java.util.HashMap;
 
 public class IntCalculator {
-
-    public enum OPERATOR {PLUS, MINUS, MULTIPLY, DIVIDE}
-
-    private interface MathOp {
+    //One arg interface
+    private interface IntOp {
+        int op(int a);
+    }
+    //Two arg interface
+    private interface IntOp2 {
         int op(int a, int b);
     }
-    private MathOp addition = (a, b) -> a + b;
-    private MathOp subtraction = (a, b) -> a - b;
-    private MathOp multiplication = (a, b) -> a * b;
-    private MathOp division = (a, b) -> a / b;
-
-    private HashMap<OPERATOR, Object> opSymbol  = new HashMap<>() {{
-        put(OPERATOR.PLUS, "+");
-        put(OPERATOR.MINUS, "-");
-        put(OPERATOR.MULTIPLY, "*");
-        put(OPERATOR.DIVIDE, "/");
+    //Lambda interface definitions
+    private IntOp  sqrt = a -> {System.out.print("\"warning integer square\" "); return (int) Math.sqrt(a); };
+    private IntOp2 power = (a, b)  -> (int) Math.pow(a, b);
+    private IntOp2 addition = Integer::sum;
+    private IntOp2 subtraction = (a, b) -> a - b;
+    private IntOp2 multiplication = (a, b) -> a * b;
+    private IntOp2 division = (a, b) -> {System.out.print("\"warning integer divide\" "); return a / b; };
+    //Map of symbols to interfaces
+    private HashMap<String, Object> opMethod  = new HashMap<>() {{
+        put("^", power);
+        put("+", addition);
+        put("-", subtraction);
+        put("*", multiplication);
+        put("/", division);
+        put("sqrt", sqrt);
     }};
-
-    private HashMap<OPERATOR, Object> opMethod  = new HashMap<>() {{
-        put(OPERATOR.PLUS, addition);
-        put(OPERATOR.MINUS, subtraction);
-        put(OPERATOR.MULTIPLY, multiplication);
-        put(OPERATOR.DIVIDE, division);
-    }};
-
-    private void intCalculator(int a, OPERATOR op, int b ) {
-        MathOp mathop = (MathOp) opMethod.get(op);
-        System.out.println(a + " " +  opSymbol.get(op) + " " +  b + " = " + mathop.op(a, b));
+    //Signature for one arg calculator
+    private void calculate(int a, String op) {
+        IntOp method = (IntOp) opMethod.get(op);
+        System.out.println(op + "(" +  a + ")" + " = " + method.op(a));
     }
+    //Signature for two arg calculator
+    private void calculate(int a, String op, int b ) {
+        IntOp2 method = (IntOp2) opMethod.get(op);
+        System.out.println(a + " " +  op + " " +  b + " = " + method.op(a, b));
+    }
+    //Tester
+    public static void main(String[] args) {
+        IntCalculator icalc = new IntCalculator();
 
-    public static void main(String args[]) {
-        IntCalculator tester = new IntCalculator();
-
-        tester.intCalculator(10, OPERATOR.PLUS, 5);
-        tester.intCalculator(10, OPERATOR.MINUS, 5);
-        tester.intCalculator(10, OPERATOR.MULTIPLY, 5);
-        tester.intCalculator(10, OPERATOR.DIVIDE, 5);
+        icalc.calculate(10, "+", 5);
+        icalc.calculate(10, "-", 5);
+        icalc.calculate(10, "*", 5);
+        icalc.calculate(10, "/", 5);
+        icalc.calculate(10, "^", 5);
+        icalc.calculate(10, "sqrt");
     }
 }
