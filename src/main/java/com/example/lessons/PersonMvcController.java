0e1c847aeaf4221b371495392b46b58f6beffb54
John Mortensen <jmortensen@powayusd.com>
2021-02-15 20:29:37 -0800

    MVC comments

--- src/main/java/com/example/lessons/PersonMvcController.java
+++ src/main/java/com/example/lessons/PersonMvcController.java
@@ -1,6 +1,6 @@
 package com.example.lessons;
 
-import com.example.lessons.models.PersonForm;
+import com.example.lessons.models.Person;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,29 +9,33 @@ import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import javax.validation.Valid;
 
+// Built using article: https://spring.io/guides/gs/validating-form-input/
 @Controller
 public class PersonMvcController implements WebMvcConfigurer {
 
+    /*
+    If all bound attribute are valid, a redirect occurs to this route and template.
+     */
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/personresults").setViewName("starters/personresults");
     }
 
     /*  The HTML template Forms and PersonForm attributes are bound
-        @return(template for person form)
-        @param(PersonForm Class)
+        @return - template for person form
+        @param - Person Class
     */
     @GetMapping("/person")
-    public String showForm(PersonForm personForm) {
+    public String showForm(Person person) {
         return "starters/person";
     }
 
     /* Gathers the attributes filled out in the form, tests for and retrieves validation error
-    @param(PersonForm object with @Valid)
-    @param(BindingResult object)
+    @param - Person object with @Valid
+    @param - BindingResult object
      */
     @PostMapping("/person")
-    public String checkData(@Valid PersonForm personForm, BindingResult bindingResult) {
+    public String checkData(@Valid Person person, BindingResult bindingResult) {
         // Validation of Decorated PersonForm attributes
         if (bindingResult.hasErrors()) {
             return "starters/person";
--- src/main/java/com/example/lessons/models/Person.java
+++ src/main/java/com/example/lessons/models/Person.java
@@ -4,11 +4,16 @@ import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
-public class PersonForm {
+public class Person {
+    /*
+    @NotNull: Does not allow a null value, which is what Spring MVC generates if the entry is empty.
+    @Size(min=2, max=30): Allows names between 2 and 30 characters long.
+     */
     @NotNull
     @Size(min = 2, max = 30)
     private String name;
 
+    // @Min(18): Does not allow the age to be less than 18.
     @NotNull
     @Min(18)
     private Integer age;
