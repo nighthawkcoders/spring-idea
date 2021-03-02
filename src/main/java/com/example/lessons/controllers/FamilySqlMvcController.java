283556f694ff442d81f3170985e51c3395feb9d6
John Mortensen <jmortensen@powayusd.com>
2021-03-02 09:02:53 -0800

    Add delete

--- src/main/java/com/example/lessons/controllers/FamilySqlMvcController.java
+++ src/main/java/com/example/lessons/controllers/FamilySqlMvcController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -67,5 +68,11 @@ public class FamilySqlMvcController implements WebMvcConfigurer {
         familySqlRepository.save(family);
         redirectAttributes.addAttribute("familyString", family.toString());
         return "redirect:/sql/familyresults";
+    }
+
+    @GetMapping("/sql/familydelete/{id}")
+    public String familyDelete(@PathVariable("id") long id) {
+        familySqlRepository.delete(id);
+        return "redirect:/sql/family";
     }
 }
